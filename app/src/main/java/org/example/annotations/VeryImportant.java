f4301d031ac680e9b7009596137102a094507959
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2024-07-15 20:07:09 +0100

    updated

--- app/src/main/java/org/example/annotations/VeryImportant.java
+++ app/src/main/java/org/example/annotations/VeryImportant.java
@@ -1,4 +1,8 @@
 package org.example.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
 public @interface VeryImportant {
 }
--- app/src/main/java/org/example/classes/User.java
+++ app/src/main/java/org/example/classes/User.java
@@ -4,4 +4,23 @@ import org.example.annotations.VeryImportant;
 
 @VeryImportant
 public class User {
+    public String name;
+    public int age;
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
